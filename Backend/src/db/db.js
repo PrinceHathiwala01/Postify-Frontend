dcaef0b07d5e4cf9cdb3a8aba536ddc18c37c1ca
Prince Hathiwala <princehathiwala76@gmail.com>
2026-04-28 19:33:06 +0530

    Delete Backend directory

--- Backend/src/db/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
-dotenv.config();
-
-async function connectDB() {
-    await mongoose.connect(`${process.env.DB_URL}/Post_Details`);
-    console.log("Connected to Database");
-}
-module.exports = connectDB;
